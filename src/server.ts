ae454dbae7a415cac4eb8ada637aebb86de23805
FlacoJones <andrew@openq.dev>
2023-08-16 16:33:09 -0500

    adds nodemon

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,28 @@ app.use(bodyParser.json());
 // Create an instance of OSSClient
 const ossClient = new OSSClient(StorageType.InMemory);
 
+const apiSecret = 'your-api-secret';
+
+// Define a middleware for authorization check
+const authorize = (req: Request, res: Response, next: any) => {
+  const authorizationHeader = req.headers['authorization'];
+  if (!authorizationHeader || authorizationHeader !== `Bearer ${apiSecret}`) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+};
+
+// Define a POST route for adding an access token
+app.post('/add-token', authorize, (req: Request, res: Response) => {
+  try {
+    const token = req.body.token as string; // Assuming the token is provided in the request body
+    ossClient.addToken(token);
+    res.json({ message: 'Token added successfully' });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Define a POST route for making requests
 app.post('/', async (req: Request, res: Response) => {
   try {
