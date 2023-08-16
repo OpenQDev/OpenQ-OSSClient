4abe3aee9b4d81ba67623b750901c05b4647038f
FlacoJones <andrew@openq.dev>
2023-08-16 14:02:34 -0500

    adds docstrings to InMemoryDequeueSet

--- src/dequeueset/InMemoryDequeueSet.ts
+++ src/dequeueset/InMemoryDequeueSet.ts
@@ -108,10 +108,22 @@ inMemoryDequeueSet.enqueue('valid_token_1');
 inMemoryDequeueSet.enqueue('valid_token_2');
 inMemoryDequeueSet.enqueue('valid_token_3');
 
-console.log(inMemoryDequeueSet);
+// [ 'valid_token_1', 'valid_token_2', 'valid_token_3' ]
+console.log(inMemoryDequeueSet.queue);
 
 if (!inMemoryDequeueSet.isEmpty()) {
   inMemoryDequeueSet.sendToBack(inMemoryDequeueSet.peek() as string);
 }
 
-console.log(inMemoryDequeueSet);
+// [ 'valid_token_2', 'valid_token_3', 'valid_token_1' ]
+console.log(inMemoryDequeueSet.queue);
+
+// valid_token_2
+console.log(inMemoryDequeueSet.peek());
+
+inMemoryDequeueSet.remove('valid_token_2');
+
+// [ 'valid_token_3', 'valid_token_1' ]
+console.log(inMemoryDequeueSet.queue);
+// valid_token_3
+console.log(inMemoryDequeueSet.peek());
